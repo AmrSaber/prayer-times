89dd6bbc321b2b237921bc3d3d7a7db84fe5bec4
Amr Saber <amr.m.saber.mail@gmail.com>
2023-11-19 01:35:11 +0000

    use <svelte:head> tag: link manifest, add title, and refactor google analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,23 @@
 <script>
-    import './global.css'
+	import './global.css';
 </script>
 
-<slot></slot>
+<svelte:head>
+	<!-- Google analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-R76D3CED01"></script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+		function gtag() {
+			dataLayer.push(arguments);
+		}
+		gtag('js', new Date());
+
+		gtag('config', 'G-R76D3CED01');
+	</script>
+
+	<link rel="manifest" href="manifest.json" />
+
+	<title>Prayer Times</title>
+</svelte:head>
+
+<slot />
